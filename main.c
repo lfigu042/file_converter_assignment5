bdd6e14f4fa08d2262c28abbce3d9cc98de18139
Laura Figueroa <lfigu042@fiu.edu>
2021-03-29 02:00:13 -0400

    finished reading input

--- main.c
+++ main.c
@@ -7,7 +7,20 @@
 #define MAX_ROW 1000    //input file will have less than or equal to 1000 lines
 #define MAX_LEN 1000
 #define MAX_TOKEN 100
-
+    char getCommandWord(char command[], int maxLength) {
+        char lastCharacter;//either space or new line
+        int i;
+        for (i = 0; (lastCharacter = getchar()) == ' '; i++);//skip leading white spaces
+        if (lastCharacter == '\n') {
+            command[0] = '\0';
+            return lastCharacter;
+        }
+        command[0] = lastCharacter;
+        for (i = 1; i < maxLength - 1 && (command[i] = getchar()) != ' ' && command[i] != '\n'; i++);
+        lastCharacter = command[i];
+        command[i] = '\0';
+        return lastCharacter;
+    }
     char* trim(char* token) {
         if (!token)     //error checking
             return NULL;
@@ -20,24 +33,72 @@
     }
 
     main() {
-        int i = 0,dataIndex = 0;
-        FILE* input = fopen("..\\mytable.csv", "r");
-        char* data[MAX_ROW];        // will store the file content
-        char* token;
-        char line[MAX_LEN];         //temporary placeholder for a line input from the file
-        for (; fscanf(input, "%[^\n]\n", line) != EOF; i++) {
-            data[dataIndex] = (char*)malloc(strlen(line) + 1);//+1 for \0
-            strcpy(data[dataIndex++], line);
+//      get user input
+        char command[MAX_TOKEN];
+        char inputFile[MAX_TOKEN];
+        char outputFile[MAX_TOKEN];
+        char inputTemp[MAX_TOKEN];
+        char outputTemp[MAX_TOKEN];
+        char *inputFileType;
+        char *outputFileType;
+
+        char lastCharacter1; //last character after receiving command word
+        char lastCharacter2; //last character after input file
+        char lastCharacter3; //last character after output file
+        printf("Welcome to my file converter \navailable commands are: \n1- quit \n2- convert source.xxx destination.yyy \nsupported file types are .csv .tc9  .tl5 .tr9\n");
+        while (1){ //infinite while loop...
+            printf("Please enter a new command... quit or convert source.xxx destination.yyy\n");
+            lastCharacter1 = getCommandWord(command, MAX_TOKEN); //get first word from user input
+            lastCharacter2 = getCommandWord(inputFile, MAX_TOKEN); //store input file
+            lastCharacter3 = getCommandWord(outputFile, MAX_TOKEN); //store output file
+
+            if (strcmp(command, "quit") == 0){ //if command === "quit"
+                printf("Closing program...");
+                break;
+            }else if (!strcmp(command, "convert") && (lastCharacter1 != '\n') && (lastCharacter2 != '\n') && (lastCharacter3 == '\n')){ //checking that input is complete
+//              determine the input file
+                printf("processing input and output files \n");
+//                use temp variables to keep inputFile and outputFile from changing
+                strcpy(inputTemp, inputFile);
+                strcpy(outputTemp, outputFile);
+
+//              get the input/output file type
+                strtok(inputTemp, ".");
+                inputFileType = strtok(NULL, ".");
+                strtok(outputTemp, ".");
+                outputFileType = strtok(NULL, ".");
+
+//              confirm all values
+                printf("input file: %s \n", inputFile);
+                printf("output file: %s \n", outputFile);
+                printf("converting from %s to %s \n",inputFileType,outputFileType );
+            }
+            else {
+                printf("\n invalid command! Your command must start either with quit or convert source.xxx destination.yyy\n");
+                while (lastCharacter1 != '\n')//skip the remainder of my invalid command...
+                    lastCharacter1 = getCommandWord(command, MAX_TOKEN);
+            }
         }
-        fclose(input);
-        FILE* output = fopen("..\\mytable.tl5", "w");
-        for (i = 0; i < dataIndex; i++) {
-            token = trim(strtok(data[i], ","));     //tokenizes the ith row stored in data[i] and trims it
-            fprintf(output, "%-5.5s", token);
-            while ((token = trim(strtok(NULL,","))))
-                fprintf(output, "|%-5.5s", token);
-            fprintf(output, i == dataIndex - 1 ? "" : "\n");
-        }
-        fclose(output);
+
+//        int i = 0,dataIndex = 0;
+//        FILE* input = fopen("..\\mytable.csv", "r");
+//        char* data[MAX_ROW];        // will store the file content
+//        char* token;
+//        char line[MAX_LEN];         //temporary placeholder for a line input from the file
+//        for (; fscanf(input, "%[^\n]\n", line) != EOF; i++) {
+//            data[dataIndex] = (char*)malloc(strlen(line) + 1);//+1 for \0
+//            strcpy(data[dataIndex++], line);
+//        }
+//        fclose(input);
+//        FILE* output = fopen("..\\mytable.tl5", "w");
+//        for (i = 0; i < dataIndex; i++) {
+//            token = trim(strtok(data[i], ","));     //tokenizes the ith row stored in data[i] and trims it
+//            fprintf(output, "%-5.5s", token);
+//            while ((token = trim(strtok(NULL,","))))
+//                fprintf(output, "|%-5.5s", token);
+//            fprintf(output, i == dataIndex - 1 ? "" : "\n");
+//        }
+//        fclose(output);
+
     }
 
